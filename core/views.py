08a8795c5922600219c3653c220fe90fc993accf
Grzegorz <grzegorz00.git@gmail.com>
2024-01-19 12:51:06 +0100

    Added product search, categories, HTML pages
    - Implemented product search functionality to enhance user experience and facilitate efficient product discovery.
    - Added product categories, enabling better organization and filtering of products for ease of navigation.
    - Created HTML files for category/shop page and product details page, providing a structured layout for displaying product categories and detailed information about individual products.

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from product.models import Category, Product
 def frontpage(request):
     products = Product.objects.all()[0:8]
     return render(request, 'core/frontpage.html', {'products': products})
+
+def shop(request):
+    products = Product.objects.all()
+    return render(request, 'core/shop.html', {'products': products})
